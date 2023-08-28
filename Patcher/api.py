0d19b34300aac5ec45decd3c9ebb9a2608a19406
AlexFalse <96215838+Alex-False@users.noreply.github.com>
2023-08-28 17:40:44 -0500

    Update api.py
    s

--- Patcher/api.py
+++ Patcher/api.py
@@ -112,4 +112,4 @@ def upload_file():
         return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, host:"0.0.0.0", port: 8080)
+    app.run(debug=True, host="0.0.0.0", port=8080)
