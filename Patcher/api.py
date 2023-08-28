6ab4208a4e66482a50e27f7f3f801b372b4b3f27
AlexFalse <96215838+Alex-False@users.noreply.github.com>
2023-08-28 18:56:08 -0500

    Update api.py

--- Patcher/api.py
+++ Patcher/api.py
@@ -13,11 +13,6 @@ import requests
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = "/home/ubuntu/patchedex/Patcher/UPLOAD_FOLDER"
-BGRM_FOLDER = "/home/ubuntu/patchedex/Patcher/BGRM_FOLDER"
-COMPARISON_FOLDER = "/home/ubuntu/patchedex/Patcher/COMPARISON_FOLDER"
-TEMP_DOWNLOAD_FOLDER = "/home/ubuntu/patchedex/Patcher/TEMP_DOWNLOAD_FOLDER"
-
 s3 = boto3.resource('s3', region_name='us-east-2')
 BUCKET = 'tobytether'
 
@@ -31,30 +26,30 @@ def upload_file():
         #The four variables above help me contatinte the file and url names for saving and retrieving. I've had problems
         # incorporating '/' in strings before so I keep it as its own variable
         test_file = request.files['Initial_Patch']
-        test_file.save(os.path.join(UPLOAD_FOLDER, test_file.filename + png))
+        test_file.save(os.path.join('UPLOAD_FOLDER', test_file.filename + png))
         #The two above lines of code take the posted file and put it in a temporary folder, also forcing it into a png format
         file_to_parse = test_file.filename + png
         #This variable just helps call from the local folder easier
         masterlist = []
         #The masterlist variable will ultimately be filled of sublists that have two elements: the url and the similarity
         # associated with the url. This will make more sense lower down
-        input = Image.open(UPLOAD_FOLDER + slasher + file_to_parse)
+        input = Image.open('UPLOAD_FOLDER' + slasher + file_to_parse)
         output = remove(input)
-        output.save(os.path.join(BGRM_FOLDER, test_file.filename + presenter + png))
+        output.save(os.path.join('BGRM_FOLDER', test_file.filename + presenter + png))
         #The above lines of code take from the temporary folder and make a new file that has the background removed
         # then saves that file to a different local folder
-        s3.Bucket(BUCKET).upload_file(BGRM_FOLDER + slasher + test_file.filename + presenter + png, "NoBg" + slasher + test_file.filename + presenter + png)
+        s3.Bucket(BUCKET).upload_file('BGRM_FOLDER' + slasher + test_file.filename + presenter + png, "NoBg" + slasher + test_file.filename + presenter + png)
         #This line just contatinates the folder locations into the bucket location. It saves from the local folder into the AWS
         # bucket that has the other images with their backgrounds removed
         imageBox = output.getbbox()
         output_boxed = output.crop(imageBox).resize((254,254))
-        output_boxed.save(os.path.join(COMPARISON_FOLDER, test_file.filename + comparisoner + png))
+        output_boxed.save(os.path.join('COMPARISON_FOLDER', test_file.filename + comparisoner + png))
         #The above lines of code do a similar operation to removing the background and saving to a local folder, only it
         # it takes from background removed folder and puts it into a comparison folder. It puts everything to the same size
         # so they may be adequetely compared.
         #The file is not saved to the AWS Bucket yet, otherwise it would inevitably be compared with itself. This line of code
         # is featured toward the end of this function
-        img2 = cv2.imread(COMPARISON_FOLDER + slasher + test_file.filename + comparisoner + png)
+        img2 = cv2.imread('COMPARISON_FOLDER' + slasher + test_file.filename + comparisoner + png)
         img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
         #These lines of code prep the initial patch to be compared
         for obj in s3.Bucket(BUCKET).objects.filter(Prefix='Square/'):
@@ -66,13 +61,13 @@ def upload_file():
             # conditional above will skip it as a first step
             url = 'https://tobytether.s3.us-east-2.amazonaws.com/Square/' + obj.key[7:]
             r = requests.get(url, allow_redirects=True)
-            open(TEMP_DOWNLOAD_FOLDER + '/' + obj.key[7:], 'wb').write(r.content)
+            open('TEMP_DOWNLOAD_FOLDER/' + obj.key[7:], 'wb').write(r.content)
             #These three lines of code will get the url of the file associated with its position in the bucket. The
             # obj.key[7:] object is the name of the file without a 'Square/' in front of it
             sublist = []
             #The sublist will be the url combined with how similar it is to the upload folder. All the sublists will
             # be added to the masterlist
-            img1_raw = TEMP_DOWNLOAD_FOLDER + '/' + obj.key[7:]
+            img1_raw = 'TEMP_DOWNLOAD_FOLDER/' + obj.key[7:]
             img1 = cv2.imread(img1_raw)
             img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
             #These three lines of code prep the comparison file to be compared to the upload file
@@ -96,20 +91,20 @@ def upload_file():
         returnlist.append('https://tobytether.s3.us-east-2.amazonaws.com/NoBg/' + test_file.filename + presenter + png)
         #This adds the original ptch at the very end so the user has the option of selecting their own, in the event theirs is
         # the first one of its variety added to the database
-        dir = BGRM_FOLDER + '/'
+        dir = 'BGRM_FOLDER/'
         for f in os.listdir(dir):
             os.remove(os.path.join(dir, f))
-        dir = COMPARISON_FOLDER + '/'
+        dir = 'COMPARISON_FOLDER/'
         for f in os.listdir(dir):
             os.remove(os.path.join(dir, f))
-        dir = TEMP_DOWNLOAD_FOLDER + '/'
+        dir = 'TEMP_DOWNLOAD_FOLDER/'
         for f in os.listdir(dir):
             os.remove(os.path.join(dir, f))
-        dir = UPLOAD_FOLDER + '/'
+        dir = 'UPLOAD_FOLDER/'
         for f in os.listdir(dir):
             os.remove(os.path.join(dir, f))
         #These lines of code purge the temporary folders of their contents for the next run-through
-        s3.Bucket(BUCKET).upload_file(COMPARISON_FOLDER + slasher + test_file.filename + comparisoner + png, "Square" + slasher + test_file.filename + comparisoner + png)
+        s3.Bucket(BUCKET).upload_file('COMPARISON_FOLDER' + slasher + test_file.filename + comparisoner + png, "Square" + slasher + test_file.filename + comparisoner + png)
         #This is the line of code that was mentioned above that was put down below. It is put here so the patch does not compare
         # itself to itself when is run through, but still put into the AWS database for future runs
         return returnlist
