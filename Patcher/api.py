e151251e3cf5de787e72c49a700fdc8647953d13
Jason Zahn <jason@rubyshore.com>
2023-09-20 14:50:59 -0500

    refactoring in progress

--- Patcher/api.py
+++ Patcher/api.py
@@ -13,7 +13,9 @@ app = Flask(__name__)
 s3 = boto3.resource('s3', region_name='us-east-2')
 BUCKET = 'af-tether'
 
-def pull_from_Square():
+
+
+def clean_folders():
     dir = 'BGRM_FOLDER/'
     for f in os.listdir(dir):
         os.remove(os.path.join(dir, f))
@@ -24,37 +26,51 @@ def pull_from_Square():
     for f in os.listdir(dir):
         os.remove(os.path.join(dir, f))
     for obj in s3.Bucket(BUCKET).objects.filter(Prefix='Square/'):
-        # TODO change to secure s3
         if obj.key == 'Square/':
             continue
         url = 'https://af-tether.s3.us-east-2.amazonaws.com/Square/' + obj.key[7:]
         r = requests.get(url, allow_redirects=True)
         open('COMPARISON_FOLDER/' + obj.key[7:], 'wb').write(r.content)
 
+
+
 @app.route('/', methods = ['POST'])
 def upload_file():
-    request_image = request.files['Initial_Patch']
-    file_to_parse = request_image.filename
-    print('initial ' + file_to_parse)
+    image_raw = request.files['Initial_Patch']
+    file_to_parse = image_raw.filename
+    print('Upload: ' + file_to_parse)
+    
     png = '.png'
-    slash = '/'
     bgrm_ext = '_bgrm'
     cm_ext = '_cm'
 
-    request_image.save(os.path.join('UPLOAD_FOLDER', file_to_parse + png))
-    input = Image.open('UPLOAD_FOLDER' + slash + file_to_parse + png)
-    output = remove(input)
-    print('removed ' + file_to_parse)
-    output.save(os.path.join('BGRM_FOLDER', file_to_parse + bgrm_ext + png))
-    s3.Bucket(BUCKET).upload_file('BGRM_FOLDER' + slash + file_to_parse + bgrm_ext + png, "NoBg" + slash + file_to_parse + bgrm_ext + png)
+    # save raw image
+    upload_path = os.path.join('UPLOAD_FOLDER', file_to_parse + png)
+    image_raw.save(upload_path)
+
+    # remove backgroud with BGRM
+    image_uploaded = Image.open(upload_path)
+    image_uploaded = remove(input)
+    print('BGRM: ' + file_to_parse)
+
+    # crop the image per BGRM bounding box, the save locall and S3
+    bgrm_path = os.path.join('BGRM_FOLDER', file_to_parse + bgrm_ext + png)
+    imageBox = image_uploaded.getbbox()
+    output_cropped = image_uploaded.crop(imageBox)
+    output_cropped.save(bgrm_path)
+    s3.Bucket(BUCKET).upload_file(bgrm_path)
     
-    imageBox = output.getbbox()
-    output_boxed = output.crop(imageBox).resize((254,254))
-    output_boxed.save(os.path.join('COMPARISON_FOLDER', file_to_parse + cm_ext + png))
-    s3.Bucket(BUCKET).upload_file('COMPARISON_FOLDER' + slash + file_to_parse + cm_ext + png, "Square" + slash + file_to_parse + cm_ext + png)
+    # create comparison image
+    comp_path = os.path.join('COMPARISON_FOLDER', file_to_parse + cm_ext + png)
+    output_resized = output_cropped.resize((254,254))
+    output_resized.save(comp_path)
+    s3.Bucket(BUCKET).upload_file(comp_path)
     
-    original_image = cv2.imread('COMPARISON_FOLDER' + slash + file_to_parse + cm_ext + png)
+    # do comparisons to generate similar image suggestions
+    original_image = cv2.imread(comp_path)
     original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+
+    # do comparisons to generate similar image suggestions
     print('before for loop ' + file_to_parse)
     masterlist = []
     for filename in os.listdir('COMPARISON_FOLDER/'):
@@ -67,6 +83,8 @@ def upload_file():
         error = ssim(comparison_image, original_image)
         sublist = [error, 'https://af-tether.s3.us-east-2.amazonaws.com/NoBg/' + filename[:-7] + bgrm_ext + png]
         masterlist.append(sublist)
+
+    # sort and arrange suggestions images
     sortedlist = sorted(masterlist, key = itemgetter(0), reverse = True)
     num_suggestions = 6
     sortedlist = sortedlist[:num_suggestions]
@@ -81,6 +99,9 @@ def upload_file():
     os.remove(dir + file_to_parse + png)
     return returnlist
 
-if __name__ == "__main__":
-    pull_from_Square()
-    app.run(debug=True, host="0.0.0.0", port=8080, processes=100, threaded=False)
+
+
+#if __name__ == "__main__":
+clean_folders()
+app.run(debug=True, host="0.0.0.0", port=8080, processes=100, threaded=False)
+
